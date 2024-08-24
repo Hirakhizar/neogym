f7e50a424c048de92a0e6466fd82127bf6817867
Hirakhizar <hirakhizarkhizarhayat@gmail.com>
2024-08-24 04:03:10 -0700

    mini gym system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/why', function () {
+    return view('why');
+});
+Route::get('/trainer', function () {
+    return view('trainer');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
